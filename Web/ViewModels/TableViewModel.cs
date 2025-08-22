45ff8a84f9a742175f34f6832ef0b4e99bb833cd
Kevin van Dijk <kevinvd05@gmail.com>
2025-08-23 00:30:24 +0200

    Made table widget customizable

---
+++ Web/ViewModels/TableViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.ViewModels;
+
+public class TableViewModel
+{
+    public string Title { get; set; } = "";
+    public List<TableColumn> Columns { get; set; } = new();
+    public IEnumerable<IDictionary<string, object?>> Rows { get; set; } = Array.Empty<IDictionary<string, object?>>();
+    public string EmptyText { get; set; } = "No data";
+}
+
+public class TableColumn
+{
+    public string Header { get; set; } = string.Empty;
+    public string Key { get; set; } = string.Empty;
+}
