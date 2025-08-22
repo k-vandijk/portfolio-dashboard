9c0a95fe1691c76ece40ee9c76c0746e1243a7db
Kevin van Dijk <kevinvd05@gmail.com>
2025-08-23 00:01:42 +0200

    Made line chart widget customizable

---
+++ Web/ViewModels/LineChartViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.ViewModels;
+
+public class LineChartViewModel
+{
+    public string Title { get; set; } = string.Empty;
+    public List<LineChartDataPoint> DataPoints { get; set; } = new ();
+}
+
+public class LineChartDataPoint
+{
+    public string Label { get; set; } = string.Empty;
+    public decimal Value { get; set; } = 0;
+}
