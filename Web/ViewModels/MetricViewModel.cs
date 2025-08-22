1c47017d965a461f47ddd94fdff5fc464fac2a78
Kevin van Dijk <kevinvd05@gmail.com>
2025-08-22 23:56:47 +0200

    Renamed Metric Description to Value

--- Web/ViewModels/MetricViewModel.cs
+++ Web/ViewModels/MetricViewModel.cs
@@ -2,6 +2,6 @@
 
 public class MetricViewModel
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
 }
