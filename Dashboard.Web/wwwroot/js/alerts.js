a12215f95e256eb25b4ae73a492183e33b0dc92f
Kevin van Dijk <kevinvd05@gmail.com>
2025-09-20 23:31:28 +0200

    Fixed date input and added 3rd party packages

---
+++ Dashboard.Web/wwwroot/js/alerts.js
@@ -0,0 +1,29 @@
+﻿function showAlert(icon, title) {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: icon,
+        title: title
+    });
+}
+
+function showConfirmationDialog(text, onConfirm) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: text,
+        icon: "warning",
+        showCancelButton: true,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            onConfirm();
+        }
+    });
+}
